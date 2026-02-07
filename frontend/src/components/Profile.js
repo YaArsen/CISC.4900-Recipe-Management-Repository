079703dc847c494578e6819f91a3772f0cea4840
YaArsen <arsen.yakubyants69@bcmail.cuny.edu>
2026-02-06 21:12:34 -0500

    updated project

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,12 +1,14 @@
 import ToggleButton from './ToggleButton';
+import Search from './Search';
+import Recipes from './Recipes';
 import { jwtDecode } from 'jwt-decode';
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
     const [user, setUser] = useState(null);
+    const [isSearchPage, setIsSearchPage] = useState(true);
+    const [isRecipesPage, setIsRecipesPage] = useState(false);
     const token = localStorage.getItem('token');
-    const navigate = useNavigate();
 
     useEffect(() => {
         if (!token) return;
@@ -14,14 +16,42 @@ const Profile = () => {
         setUser(user);
     }, [token]);
 
+    useEffect(() => {
+        if (isClicked) {
+        setIsRecipesPage(true);
+        setIsSearchPage(false);
+        setIsClicked(false);
+        }
+    }, [isClicked, setIsClicked]);
+
+
     if (!token || !user) return <p>Loading...</p>;
 
     return (
-        <>
+        <div className='profile-header'>
             <h2>Welcome, {user.name}</h2>
             <ToggleButton />
-            <a href={'/search'}>Search</a>
-        </>
+            <button
+                className='search-page-btn' 
+                onClick={() => {
+                    setIsSearchPage(true);
+                    setIsRecipesPage(false);
+                }}
+            >
+                Search
+            </button>
+            <button
+                className='recipes-page-btn' 
+                onClick={() => {
+                    setIsRecipesPage(true);
+                    setIsSearchPage(false);
+                }}
+            >
+                Recipes
+            </button>
+            {isSearchPage && <Search userId={user._id} />}
+            {isRecipesPage && <Recipes userId={user._id} />}
+        </div>
     );
 };
 
