844b010da1788b5f04002c90e9143fa92472aeea
YaArsen <arsen.yakubyants69@bcmail.cuny.edu>
2026-02-06 20:58:15 -0500

    Recipes

--- frontend/src/components/Recipes.js
+++ frontend/src/components/Recipes.js
@@ -23,8 +23,31 @@ const Recipes = () => {
 
     return (
         <>
+            <div className='recipesobj'></div>
             <h3>Your recipes</h3>
             <button onClick={() => navigate('/add-recipe')}></button>
+
+            {recipes.length === 0 ? (
+                <p>No recipes yet!</p>
+            ) : (
+                <ul>
+                    {recipes.map((recipe) => (
+                        <li key={recipe._id}>
+                            <img src={recipe.photoReference} alt={recipe.title} style={{ maxWidth: '100px' }} />
+                            <h4>{recipe.title}</h4>
+                            {recipe.isPublic ? <h4>PUBLIC</h4> : <h4>PRIVATE</h4>}
+                            <h4>{recipe.username}</h4>
+                            <h4>{new Date(recipe.timestamp).toLocaleTimeString()}</h4>
+                            <div>
+                                <button onClick={() => navigate(`/recipe/${userId}/${recipe._id}`)}>View</button>
+                                <button onClick={() => navigate(`/edit-recipe/${recipe._id}`)}>Edit</button>
+                                <DeleteRecipe recipeId={recipe._id} setRecipes={setRecipes} />
+                                <button onClick={() => navigate(`/comments/${userId}/${recipe._id}`)}>Comments</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </>
     );
 };
